113bce668b8f89a381a0efa8e98f69b978a1f953
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-04-11 01:27:06 -0300

    refactor: replace ActivityIndicator with styled LoadIndicator in Loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,14 +1,13 @@
-import { ActivityIndicator } from 'react-native'
 import { useTheme } from 'styled-components/native'
 
-import { Container } from './styles'
+import { Container, LoadIndicator } from './styles'
 
 export function Loading() {
   const { COLORS } = useTheme()
 
   return (
     <Container>
-      <ActivityIndicator size="large" color={COLORS.GREEN_700} />
+      <LoadIndicator color={COLORS.GREEN_700} />
     </Container>
   )
 }
--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -6,3 +6,7 @@ export const Container = styled.View`
   align-items: center;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 `
+
+export const LoadIndicator = styled.ActivityIndicator.attrs(({ theme }) => ({
+  color: theme.COLORS.GREEN_700,
+}))``
